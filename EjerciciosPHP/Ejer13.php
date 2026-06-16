80a92f4834b7d2aa0a080e3d1e48e7fb7d464608
Matias Vallejos <mati2000852@gmail.com>
2026-06-16 18:31:27 -0300

    Ejer 13 incompleto

---
+++ EjerciciosPHP/Ejer13.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+    <title>Ejercicio 11</title>
+    </head>
+    <body>
+    <?php
+        class Tabla {
+            private $nroCelda;
+            private $valor;
+
+            function
+        }
+
+
+    </body>
+</html>
